2c01e108f34bdc1f4a01c2cfb1ccc7e727036b7c
Costas Vrahimis <cvrahimis.11@gmail.com>
2016-05-26 21:18:09 -0400

    Initial Commit

---
+++ DaysInState/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DaysInState
+//
+//  Created by Costas Vrahimis on 5/26/16.
+//  Copyright © 2016 Costas Vrahimis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
