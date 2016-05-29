0cc0c2b00c91d1bd100c57e9b684241b6e34a146
Costas Vrahimis <cvrahimis.11@gmail.com>
2016-05-29 12:09:24 -0400

    added functionality to infoVC

--- DaysInState/InfoViewController.swift
+++ DaysInState/InfoViewController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class InfoViewController: UIViewController, UITextFieldDelegate{
+class InfoViewController: UIViewController, UITextFieldDelegate, UIScrollViewDelegate{
     var screenWidth: CGFloat =  0.0
     var screenHeight: CGFloat = 0.0
     
@@ -22,6 +22,7 @@ class InfoViewController: UIViewController, UITextFieldDelegate{
     @IBOutlet var state: UITextField!
     @IBOutlet var zip: UITextField!
     @IBOutlet var label: UILabel!
+    @IBOutlet var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,26 +36,40 @@ class InfoViewController: UIViewController, UITextFieldDelegate{
         mainView.backgroundColor = UIColor.redColor()
         mainView.userInteractionEnabled = true
         
+        scrollView = UIScrollView(frame: CGRectMake(0, 0, screenWidth, screenHeight))
+        //scrollView.center = CGPointMake(screenWidth * 0.5, screenHeight)
+        scrollView.backgroundColor = UIColor.blueColor()
+        scrollView.pagingEnabled = false
+        scrollView.scrollEnabled = true
+        scrollView.userInteractionEnabled = true
+        scrollView.showsVerticalScrollIndicator = false
+        let touch = UITapGestureRecognizer(target:self, action:#selector(InfoViewController.scrollViewTap))
+        scrollView.addGestureRecognizer(touch)
+        scrollView.contentSize = CGSizeMake(screenWidth, screenHeight * 1.3)
+        scrollView.contentOffset = CGPoint(x: 0, y: 0)
+        
         firstName = UITextField(frame: CGRectMake(0, 0, screenWidth * 0.75, screenHeight * 0.075))
-        firstName.center = CGPointMake(screenWidth * 0.5, screenHeight * 0.2)
+        firstName.center = CGPointMake(screenWidth * 0.5, screenHeight * 0.1)
         firstName.delegate = self
         firstName.layer.cornerRadius = 10
         firstName.clipsToBounds = true
         firstName.placeholder = " First Name"
         firstName.backgroundColor = UIColor.whiteColor()
-        mainView.addSubview(firstName)
+        firstName.tag = 0
+        scrollView.addSubview(firstName)
         
         lastName = UITextField(frame: CGRectMake(0, 0, screenWidth * 0.75, screenHeight * 0.075))
-        lastName.center = CGPointMake(screenWidth * 0.5, screenHeight * 0.3)
+        lastName.center = CGPointMake(screenWidth * 0.5, screenHeight * 0.2)
         lastName.delegate = self
         lastName.layer.cornerRadius = 10
         lastName.clipsToBounds = true
         lastName.placeholder = " Last Name"
         lastName.backgroundColor = UIColor.whiteColor()
-        mainView.addSubview(lastName)
+        lastName.tag = 1
+        scrollView.addSubview(lastName)
         
         label = UILabel(frame: CGRectMake(0, 0, screenWidth * 0.9, screenHeight * 0.2))
-        label.center = CGPointMake(screenWidth * 0.5, screenHeight * 0.4)
+        label.center = CGPointMake(screenWidth * 0.5, screenHeight * 0.3)
         label.backgroundColor = UIColor.clearColor()
         label.text = "Please input your name and primary Address"
         label.lineBreakMode = .ByWordWrapping // or NSLineBreakMode.ByWordWrapping
@@ -64,55 +79,101 @@ class InfoViewController: UIViewController, UITextFieldDelegate{
         label.textAlignment = NSTextAlignment.Center
         //label.layer.borderColor = UIColor( red:255/100, green:255/100, blue:255/100, alpha: 1.0 ).CGColor
         //label.layer.borderWidth = 3
-        mainView.addSubview(label)
+        scrollView.addSubview(label)
         
         address1 = UITextField(frame: CGRectMake(0, 0, screenWidth * 0.75, screenHeight * 0.075))
-        address1.center = CGPointMake(screenWidth * 0.5, screenHeight * 0.5)
+        address1.center = CGPointMake(screenWidth * 0.5, screenHeight * 0.4)
         address1.delegate = self
         address1.layer.cornerRadius = 10
         address1.clipsToBounds = true
         address1.placeholder = " Address line 1"
         address1.backgroundColor = UIColor.whiteColor()
-        mainView.addSubview(address1)
+        address1.tag = 2
+        scrollView.addSubview(address1)
         
         address2 = UITextField(frame: CGRectMake(0, 0, screenWidth * 0.75, screenHeight * 0.075))
-        address2.center = CGPointMake(screenWidth * 0.5, screenHeight * 0.6)
+        address2.center = CGPointMake(screenWidth * 0.5, screenHeight * 0.5)
         address2.delegate = self
         address2.layer.cornerRadius = 10
         address2.clipsToBounds = true
         address2.placeholder = " Address line 2"
         address2.backgroundColor = UIColor.whiteColor()
-        mainView.addSubview(address2)
+        address2.tag = 3
+        scrollView.addSubview(address2)
         
         city = UITextField(frame: CGRectMake(0, 0, screenWidth * 0.75, screenHeight * 0.075))
-        city.center = CGPointMake(screenWidth * 0.5, screenHeight * 0.7)
+        city.center = CGPointMake(screenWidth * 0.5, screenHeight * 0.6)
         city.delegate = self
         city.layer.cornerRadius = 10
         city.clipsToBounds = true
         city.placeholder = " City"
         city.backgroundColor = UIColor.whiteColor()
-        mainView.addSubview(city)
+        city.tag = 4
+        scrollView.addSubview(city)
         
         state = UITextField(frame: CGRectMake(0, 0, screenWidth * 0.75, screenHeight * 0.075))
-        state.center = CGPointMake(screenWidth * 0.5, screenHeight * 0.8)
+        state.center = CGPointMake(screenWidth * 0.5, screenHeight * 0.7)
         state.delegate = self
         state.layer.cornerRadius = 10
         state.clipsToBounds = true
         state.placeholder = " State"
         state.backgroundColor = UIColor.whiteColor()
-        mainView.addSubview(state)
+        state.tag = 5
+        scrollView.addSubview(state)
         
         zip = UITextField(frame: CGRectMake(0, 0, screenWidth * 0.75, screenHeight * 0.075))
-        zip.center = CGPointMake(screenWidth * 0.5, screenHeight * 0.9)
+        zip.center = CGPointMake(screenWidth * 0.5, screenHeight * 0.8)
         zip.delegate = self
         zip.layer.cornerRadius = 10
         zip.clipsToBounds = true
         zip.placeholder = " Zip"
         zip.backgroundColor = UIColor.whiteColor()
-        mainView.addSubview(zip)
+        zip.tag = 6
+        scrollView.addSubview(zip)
        
+        mainView.addSubview(scrollView)
         self.view.addSubview(mainView)
     }
+    
+    func scrollViewTap(){
+        view.endEditing(true)
+        scrollView.contentOffset = CGPoint(x: 0, y: -64)//find mathamatical way to calculate 64
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        scrollView.contentOffset = CGPoint(x: 0, y: (Int(textField.bounds.height) * textField.tag))
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool
+    {
+        switch textField.tag
+        {
+            case 0:
+                lastName.becomeFirstResponder()
+                lastName.delegate = self
+            case 1:
+                address1.becomeFirstResponder()
+                address1.delegate = self
+            case 2:
+                address2.becomeFirstResponder()
+                address2.delegate = self
+            case 3:
+                city.becomeFirstResponder()
+                city.delegate = self
+            case 4:
+                state.becomeFirstResponder()
+                state.delegate = self
+            case 5:
+                zip.becomeFirstResponder()
+                zip.delegate = self
+            default:
+                self.view.endEditing(true)
+                scrollView.contentOffset = CGPoint(x: 0, y: -64) //find mathamatical way to calculate 64
+        }
+        
+        return false;
+    }
+
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
